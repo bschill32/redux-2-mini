eb558633bdeb2b7d35b833f1c0a68316ba739fa5
Brendan Schiller <schiller.brendan@gmail.com>
2019-04-10 14:00:19 -0600

    working

---
+++ __instructor-code__/src/components/HackerNews/HackerNews.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Card from './../shared/Card/Card';
+import Loading from './../shared/Loading/Loading';
+import { requestArticles } from './../../ducks/hackerNewsReducer';
+import { connect } from 'react-redux';
+
+class HackerNews extends Component {
+  componentDidMount() {
+    this.props.requestArticles()
+  }
+  render() {
+    const articles = this.props.articles.map((article => <Card key={article.id} article={article} />))
+    return (
+      <div className='news-container'>
+        <img style={styles.logo} src="./hackerNews.jpeg" alt="" />
+        {this.props.loading ? <Loading /> : <div>{articles}</div>}
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(reduxStoreState) {
+  return reduxStoreState.hackerNews
+}
+
+// this.props = {...this.props, ...reduxStoreState}
+
+export default connect(mapStateToProps, { requestArticles })(HackerNews);
+
+
+const styles = {
+  logo: {
+    width: '250px',
+    margin: '50px 0px'
+  }
+}
--- src/components/Medium/Medium.js
+++ src/components/Medium/Medium.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import Card from './../shared/Card/Card';
-import Loading from './../shared/Loading/Loading';
+import React, { Component } from "react"
+import Card from "./../shared/Card/Card"
+import Loading from "./../shared/Loading/Loading"
+import { connect } from "react-redux"
+import { getMediumArticles } from "./../../ducks/mediumReducer"
 
 class Medium extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { articles: [], loading: true }
+  componentDidMount() {
+    this.props.getMediumArticles()
   }
+
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    const articles = this.props.articles.map(article => (
+      <Card key={article.id} article={article} />
+    ))
     return (
-      <div className='news-container'>
+      <div className="news-container">
         <img src="./mediumLogo.png" style={styles.logo} alt="" />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+        {this.props.loading ? <Loading /> : <div>{articles}</div>}
       </div>
     )
   }
 }
 
-export default Medium;
+function mapStateToProps(state) {
+  return state.medium
+}
+
+export default connect(
+  mapStateToProps,
+  { getMediumArticles }
+)(Medium)
 
 const styles = {
-  logo: { width: '250px' }
+  logo: { width: "250px" }
 }
